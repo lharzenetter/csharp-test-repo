7c64053a189956f49f4fea425f3987ac049fe159
Lukas Harzenetter <Lukas.Harzenetter@leuze.com>
2023-08-14 09:03:55 +0200

    add second method

---
+++ GrpcGreeter/Services/EvenOddCheckerService.cs
@@ -0,0 +1,10 @@
+namespace GrpcGreeter.Services;
+
+public class EvenOddCheckerService {
+	
+	public bool isEven(int value) {
+		return value % 2 == 0;
+	}
+
+	public bool isOdd(int value) {  return value % 2 == 1; }
+}
